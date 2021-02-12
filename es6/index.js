83dc27aefbca58d24a852b9a42c4315e2a53c0c5
Dan Appleyard <dlappleyard@gmail.com>
2021-02-11 22:43:42 -0600

    finished es6 course

--- es6/index.js
+++ es6/index.js
@@ -751,5 +751,46 @@ for(let c of tree){
 }
 values;
 
+//section 17 (promises and fetch)
+
+let promise = new Promise((resolve, reject) => {
+    resolve();
+});
+
+promise
+    .then(() => console.log('finally finished!'))
+    .then(() => console.log('i was also ran.'))
+    .catch(() => console.log('uh oh!'));
+
+//async with promises
+
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve();
+    }, 3000);
+});
+
+promise
+    .then(() => console.log('finally finished!'))
+    .then(() => console.log('i was also ran.'))
+    .catch(() => console.log('uh oh!'));
+
+//ajax requests with fetch
+
+const url = "https://jsonplaceholder.typicode.com/posts/";
+console.log("about to fetch");
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data));
+console.log("fetch called");
+
+//shortcomings of fetch
+const url = "https://jsonplaceholder.typicode.com/posts123456/";
+console.log("about to fetch");
+fetch(url)
+    .then(response => console.log(response))
+    .catch(error => console.log('BAD', error));
+
+
 
 
